031ddfa3f04a5f61733f59e4cfa51f6d5a9cabae
Enzotekrennes <enzo.monnier@epitech.eu>
2025-05-13 01:21:41 +0200

    Remove unused useNavigate import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import { useContactForm } from '../hooks/useContactForm.ts';
 import Notification from '../components/Notification.tsx';
 
