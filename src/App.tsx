16d61e3e098fda4b4888694c26831740a87b80a1
Enzotekrennes <enzo.monnier@epitech.eu>
2025-05-29 02:02:35 +0200

    feat: wrap App component with HelmetProvider for improved SEO management

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Home from './pages/Home';
 import DebugService from './pages/services/DebugService';
 import AutomatisationService from './pages/services/AutomatisationService';
@@ -15,15 +16,17 @@ function ScrollToTop() {
 }
 
 const App: React.FC = () => (
-  <Router>
-    <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/services/debug" element={<DebugService />} />
-      <Route path="/services/automatisation" element={<AutomatisationService />} />
-      <Route path="/services/outils" element={<OutilsService />} />
-    </Routes>
-  </Router>
+  <HelmetProvider>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services/debug" element={<DebugService />} />
+        <Route path="/services/automatisation" element={<AutomatisationService />} />
+        <Route path="/services/outils" element={<OutilsService />} />
+      </Routes>
+    </Router>
+  </HelmetProvider>
 );
 
 export default App; 
